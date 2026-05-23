5794bf8c6fdc320d7430928b09c65fe6b0638f0c
Pruthvi G Karigiri <pruthvigkarigiri@gmail.com>
2026-05-23 19:51:08 +0530

    Adds the base files

---
+++ schema.sql
@@ -0,0 +1,33 @@
+
+create table groups (
+ id bigint generated always as identity primary key,
+ name text not null,
+ invite_code text unique not null,
+ owner_uuid uuid not null
+);
+
+create table group_members (
+ id bigint generated always as identity primary key,
+ group_id bigint references groups(id) on delete cascade,
+ user_uuid uuid not null,
+ user_email text not null
+);
+
+alter table groups enable row level security;
+alter table group_members enable row level security;
+
+create policy "group select"
+on groups for select
+using (true);
+
+create policy "group insert"
+on groups for insert
+with check (auth.uid() = owner_uuid);
+
+create policy "member select"
+on group_members for select
+using (auth.uid() = user_uuid);
+
+create policy "member insert"
+on group_members for insert
+with check (auth.uid() = user_uuid);
